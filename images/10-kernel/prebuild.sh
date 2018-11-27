fac7b10572c40131f68691ef7dcaf11e9010148f
niusmallnan <niusmallnan@gmail.com>
2018-11-27 15:13:12 +0800

    Support for compiling multiple ARCH

--- images/10-kernel/prebuild.sh
+++ images/10-kernel/prebuild.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -ex
 
-# linux-${KERNEL_VERSION}-x86.tar
+# linux-${KERNEL_VERSION}-${KERNEL_ARCH}.tar
 
-tar -xf /source/dist/kernel/linux-${KERNEL_VERSION}-x86.tar.gz  ./boot/vmlinuz-${KERNEL_VERSION} ./boot/System.map-${KERNEL_VERSION}
-mv ./boot/vmlinuz-${KERNEL_VERSION} ./images/10-kernel/kernel
+VMLINUX_FILE="./boot/vmlinuz-${KERNEL_VERSION}"
+if [ ! -e "${VMLINUX_FILE}" ]; then
+    VMLINUX_FILE="./boot/vmlinux-${KERNEL_VERSION}"
+fi
+
+tar -xf /source/dist/kernel/linux-${KERNEL_VERSION}-${KERNEL_ARCH}.tar.gz ${VMLINUX_FILE} ./boot/System.map-${KERNEL_VERSION}
+mv ${VMLINUX_FILE} ./images/10-kernel/kernel
 mv ./boot/System.map-${KERNEL_VERSION} ./images/10-kernel/System.map
 rmdir boot
 
-cp /source/dist/kernel/linux-${KERNEL_VERSION}-x86.tar.gz ./images/10-kernel/kernel.tar.gz
+cp /source/dist/kernel/linux-${KERNEL_VERSION}-${KERNEL_ARCH}.tar.gz ./images/10-kernel/kernel.tar.gz
 gunzip -f ./images/10-kernel/kernel.tar.gz
 
 ls -lah ./images/10-kernel
